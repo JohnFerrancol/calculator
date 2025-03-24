8a529db1caf6f547a4088f52fabb92b0d044d7e0
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-03-24 11:14:26 +0800

    Added functionality to handle key press and refactored code to be have a centralised function to handle user inputs

--- script.js
+++ script.js
@@ -4,7 +4,171 @@ const OPERANDS = ["+", "-", "x", "÷", "%"];
 const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Storing the expressionContainer into an expression variable for readability of code
-let expression = expressionContainer.textContent;
+let expression = "0";
+
+// Register the button clicked and run the processInput function
+buttonContainer.addEventListener("click", (event) => {
+  const buttonText = event.target.textContent;
+  processInput(buttonText);
+});
+
+// Register the key pressed and run the processInput function
+document.addEventListener("keydown", (event) => {
+  // Change the key Input to batch with the button text to make it easier for the process input to be used with buttons and keyboard inputs
+  const changeKeyInput = {
+    "*": "x",
+    "/": "÷",
+    Enter: "=",
+    Escape: "AC",
+    Backspace: "⌫",
+  };
+
+  // If the key pressed is inside the changeKeyInput object, then change it, else do not
+  const keyPressed = changeKeyInput[event.key]
+    ? changeKeyInput[event.key]
+    : event.key;
+
+  processInput(keyPressed);
+});
+
+// Function used to handle the input given by the user from the input given
+function processInput(input) {
+  // Run the following functions for each input registered
+  if (NUMBERS.includes(input)) {
+    handleNumbers(input);
+  } else if (OPERANDS.includes(input) || input === "=") {
+    handleOperands(input);
+  } else if (input === "AC") {
+    clearCalculator();
+  } else if (input === ".") {
+    handleDecimals();
+  } else if (input === "+/-") {
+    toggleSign();
+  } else if (input === "⌫") {
+    handleBackspace();
+  }
+
+  // update the expressionContainer's text content every time an input is processed
+  updateDisplay();
+}
+
+// Handling number inputs
+function handleNumbers(input) {
+  if (NUMBERS.includes(input)) {
+    // If the expression is 0 or invalid override with the number, else append the number to the expression
+    if (isEmptyExpression() || isInvalidExpression()) {
+      expression = input;
+    } else {
+      expression += input;
+    }
+  }
+}
+
+// Handling operands button press
+function handleOperands(input) {
+  // Check if the expressions has any negative signs to prevent confusion between signs and operands
+  let containsSign = false;
+  let expressionArray = expression.split(" ");
+
+  // Set the containsSign variable to true if there are any negative signs
+  if (expressionArray.length === 1) {
+    containsSign = expressionArray[0].startsWith("-") ? true : false;
+  } else if (expressionArray.length === 3) {
+    containsSign = expressionArray[0].startsWith("-") ? true : false;
+    containsSign = expressionArray[2].startsWith("-") ? true : false;
+  }
+
+  // Do not run operations when the expression shows invalid expression
+  if (!isInvalidExpression()) {
+    if (input === "=") {
+      // Run the evaluateExpression to get the expression
+      let result = evaluateExpression(expression);
+
+      // If the result is not valid, it is due to not having a complete expression (No two numbers and operand)
+      if (!(result === "invalid expression")) {
+        expression = String(result);
+      }
+    }
+
+    // If it contains operands do the following
+    else if (OPERANDS.some((op) => expression.includes(op)) && !containsSign) {
+      // If the last button pressed is an operand, allow the user to change the operand if a different operand is pressed
+      if (OPERANDS.includes(expression[expression.length - 2])) {
+        expression = expression.slice(0, -3) + ` ${input} `;
+      }
+
+      // If a complete expression is already there and a user pressed another operand, evaluate the current expression and append the operand the user pressed
+      else {
+        let result = evaluateExpression(expression);
+        expression = `${String(result)} ${input} `;
+      }
+    }
+
+    // Else, just add the operand
+    else {
+      expression += ` ${input} `;
+    }
+  }
+}
+
+// Handling when the  user wants to clear the expression
+function clearCalculator() {
+  expression = "0";
+}
+
+// Handlign when the user wants to make a decimal/floating point number
+function handleDecimals() {
+  // Selecting the current number that the user is editing
+  let expressionArray = expression.split(" ");
+  let num1 = expressionArray.length === 1 ? expressionArray[0] : "";
+  let num2 = expressionArray.length === 3 ? expressionArray[2] : "";
+
+  // Make sure that the current number does not include a floating point
+  if (!(num1.includes(".") || num2.includes("."))) {
+    // Automatically add a 0 infront if the number is empty, else just append the floating point
+    if (OPERANDS.includes(expression[expression.length - 2])) {
+      expression += "0.";
+    } else {
+      expression += ".";
+    }
+  }
+}
+
+// Handling when the user wants to change the sign of the number
+function toggleSign() {
+  let expressionArray = expression.split(" ");
+
+  // Handle changing signs, select the current number and change the sign
+  if (expressionArray.length === 1) {
+    expression = expression.startsWith("-")
+      ? expression.slice(1)
+      : `-${expression}`;
+  } else if (expressionArray.length === 3) {
+    let lastIndex = expressionArray.length - 1;
+    expressionArray[lastIndex] = expressionArray[lastIndex].startsWith("-")
+      ? expressionArray[lastIndex].slice(1)
+      : `-${expressionArray[lastIndex]}`;
+    expression = expressionArray.join(" ");
+  }
+}
+
+// Handling when the user wants to use the backspace
+function handleBackspace() {
+  // Do not allow backspace for invalid expression
+  if (!isInvalidExpression()) {
+    // Remove additional characters for operands as there is extra spaces
+    if (OPERANDS.includes(expression[expression.length - 2])) {
+      expression = expression.slice(0, -3);
+    } else {
+      expression = expression.slice(0, -1);
+    }
+  }
+
+  // If the expression becomes empty, set the cleasr the calculator
+  if (expression === "") {
+    clearCalculator();
+  }
+}
 
 // Updating a display after changing the expression
 function updateDisplay() {
@@ -20,131 +184,6 @@ function isEmptyExpression() {
 function isInvalidExpression() {
   return expression === "Error! Press AC/Number";
 }
-
-buttonContainer.addEventListener("click", (event) => {
-  const buttonText = event.target.textContent;
-
-  // Handling number button press
-  if (NUMBERS.includes(buttonText)) {
-    // If the expression is 0 or invalid override with the number, else append the number to the expression
-    if (isEmptyExpression() || isInvalidExpression()) {
-      expression = buttonText;
-    } else {
-      expression += buttonText;
-    }
-  }
-
-  // Handling operands button press
-  else if (OPERANDS.includes(buttonText) || buttonText === "=") {
-    // Check if the expressions has any negative signs to prevent confusion between signs and operands
-    let containsSign = false;
-    let expressionArray = expression.split(" ");
-
-    // Set the containsSign variable to true if there are any negative signs
-    if (expressionArray.length === 1) {
-      containsSign = expressionArray[0].startsWith("-") ? true : false;
-    } else if (expressionArray.length === 3) {
-      containsSign = expressionArray[0].startsWith("-") ? true : false;
-      containsSign = expressionArray[2].startsWith("-") ? true : false;
-    }
-
-    // Do not run operations when the expression shows invalid expression
-    if (!isInvalidExpression()) {
-      if (buttonText === "=") {
-        // Run the evaluateExpression to get the expression
-        let result = evaluateExpression(expression);
-
-        // If the result is not valid, it is due to not having a complete expression (No two numbers and operand)
-        if (!(result === "invalid expression")) {
-          expression = String(result);
-        }
-      }
-
-      // If it contains operands do the following
-      else if (
-        OPERANDS.some((op) => expression.includes(op)) &&
-        !containsSign
-      ) {
-        // If the last button pressed is an operand, allow the user to change the operand if a different operand is pressed
-        if (OPERANDS.includes(expression[expression.length - 2])) {
-          expression = expression.slice(0, -3) + ` ${buttonText} `;
-        }
-
-        // If a complete expression is already there and a user pressed another operand, evaluate the current expression and append the operand the user pressed
-        else {
-          let result = evaluateExpression(expression);
-          expression = `${String(result)} ${buttonText} `;
-        }
-      }
-
-      // Else, just add the operand
-      else {
-        expression += ` ${buttonText} `;
-      }
-    }
-  }
-
-  // Handling when user wants to clear
-  else if (buttonText === "AC") {
-    expression = "0";
-  }
-
-  // Handling decimal
-  else if (buttonText === ".") {
-    // Selecting the current number that the user is editing
-    let expressionArray = expression.split(" ");
-    let num1 = expressionArray.length === 1 ? expressionArray[0] : "";
-    let num2 = expressionArray.length === 3 ? expressionArray[2] : "";
-
-    // Make sure that the current number does not include a floating point
-    if (!(num1.includes(".") || num2.includes("."))) {
-      // Automatically add a 0 infront if the number is empty, else just append the floating point
-      if (OPERANDS.includes(expression[expression.length - 2])) {
-        expression += "0.";
-      } else {
-        expression += ".";
-      }
-    }
-  }
-
-  // Handling changing signs
-  else if (buttonText === "+/-") {
-    let expressionArray = expression.split(" ");
-
-    // Handle changing signs, select the current number and change the sign
-    if (expressionArray.length === 1) {
-      expression = expression.startsWith("-")
-        ? expression.slice(1)
-        : `-${expression}`;
-    } else if (expressionArray.length === 3) {
-      let lastIndex = expressionArray.length - 1;
-      expressionArray[lastIndex] = expressionArray[lastIndex].startsWith("-")
-        ? expressionArray[lastIndex].slice(1)
-        : `-${expressionArray[lastIndex]}`;
-      expression = expressionArray.join(" ");
-    }
-  }
-
-  // Handling backspace
-  else if (buttonText === "⌫") {
-    // Do not allow backspace for invalid expression
-    if (!isInvalidExpression()) {
-      // Remove additional characters for operands as there is extra spaces
-      if (OPERANDS.includes(expression[expression.length - 2])) {
-        expression = expression.slice(0, -3);
-      } else {
-        expression = expression.slice(0, -1);
-      }
-    }
-
-    // If the expression becomes empty, set the expression to 0
-    if (expression === "") {
-      expression = "0";
-    }
-  }
-
-  updateDisplay();
-});
 
 // Function used to evaluate a expression string
 function evaluateExpression(evaluationString) {
